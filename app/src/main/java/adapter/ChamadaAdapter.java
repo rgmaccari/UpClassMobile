85f8c05c5bb339129e1376a44dc8f7c496adb5d4
Rafael Maccari <r.maccari@edu.unipar.br>
2024-10-28 12:27:33 -0300

    recycle view

---
+++ app/src/main/java/adapter/ChamadaAdapter.java
@@ -0,0 +1,62 @@
+package adapter;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.trabalho2obimestre.R;
+
+import java.util.List;
+import model.Aluno;
+
+public class ChamadaAdapter extends RecyclerView.Adapter<ChamadaAdapter.ViewHolder>{
+    private List<Aluno> listaAlunos;
+    private Context context;
+
+    // Construtor: recebe uma lista de alunos e pega um objeto Aluno para adicionar ao Adaptador
+    public ChamadaAdapter(List<Aluno>listaAlunos, Context context){
+        this.listaAlunos = listaAlunos;
+        this.context = context;
+    }
+
+    //Carrega os elementos XML para item da lista:
+    @NonNull
+    @Override
+    public ChamadaAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+        View itemList = inflater.inflate(R.layout.item_chamada, parent, false);
+        return new ViewHolder(itemList);
+    }
+
+    // Aciona os dados do aluno em tela:
+    @Override
+    public void onBindViewHolder(@NonNull ChamadaAdapter.ViewHolder holder, int position) {
+
+    }
+
+    // Quantidade de elementos da lista.
+    @Override
+    public int getItemCount() {
+        return this.listaAlunos.size();
+    }
+
+    //Vincular os elementos XML para cada posição da lista:
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        public TextView alunoRa;
+        public TextView tvNome;
+        public CheckBox checkboxPresenca;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            this.alunoRa = itemView.findViewById(R.id.alunoRa);
+            this.tvNome = itemView.findViewById(R.id.tvNome);
+            this.checkboxPresenca = itemView.findViewById(R.id.checkboxPresenca);
+        }
+    }
+}
