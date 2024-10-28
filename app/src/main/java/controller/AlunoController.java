4560d0d7a8a4d7cc4aabb8cb2385c4aeeaba8e5b
Rafael Maccari <r.maccari@edu.unipar.br>
2024-10-28 13:27:19 -0300

    add controller

--- app/src/main/java/controller/AlunoController.java
+++ app/src/main/java/controller/AlunoController.java
@@ -1,4 +1,24 @@
 package controller;
 
+import android.content.Context;
+
+import java.util.List;
+
+import model.Aluno;
+
 public class AlunoController {
+
+    private Context context;
+
+    public AlunoController(Context context) {
+        this.context = context;
+    }
+
+    public String salvarAluno(){
+        return null;
+    }
+
+    public List<Aluno> retornarTodosAlunos(){
+        return null;
+    }
 }
