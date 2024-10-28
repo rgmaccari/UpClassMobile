fb8f45b87fab7fd281d5ff1760d8b015c3ed4a2a
Rafael Maccari <r.maccari@edu.unipar.br>
2024-10-27 21:57:00 -0300

    activities criadas

---
+++ app/src/main/java/com/example/trabalho2obimestre/ChamadaActivity.java
@@ -0,0 +1,20 @@
+package com.example.trabalho2obimestre;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class ChamadaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_chamada);
+
+    }
+}
