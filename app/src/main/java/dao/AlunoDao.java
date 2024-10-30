84fd0baf06da013644a9d6b61ebac3d39e709860
Daniel <danielmodestop@hotmail.com>
2024-10-30 19:47:50 -0300

    Oii

--- app/src/main/java/dao/AlunoDao.java
+++ app/src/main/java/dao/AlunoDao.java
@@ -38,6 +38,7 @@ public class AlunoDao implements IGenericDao<Aluno>{
         }
     }
 
+    //ALO
     private AlunoDao(Context context){
         this.context = context;
         openHelper = new SQLiteDataHelper(this.context, "Aluno.db", null, 1);
