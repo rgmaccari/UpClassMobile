15f7b74c409baa85b23e04aa60165ef9fd01d64d
Marcelo <guedesmarcello733@outlook.com>
2024-11-02 12:13:12 -0300

    Criado e implementado o SeedData.java

--- app/src/main/java/com/example/trabalho2obimestre/helper/SQLiteDataHelper.java
+++ app/src/main/java/com/example/trabalho2obimestre/helper/SQLiteDataHelper.java
@@ -12,6 +12,7 @@ import com.example.trabalho2obimestre.model.ItemChamada;
 import java.util.ArrayList;
 
 public class SQLiteDataHelper extends SQLiteOpenHelper {
+
     public SQLiteDataHelper(@Nullable Context context, @Nullable String name,
                             @Nullable SQLiteDatabase.CursorFactory factory, int version){
         super(context, name, factory, version);
@@ -22,17 +23,20 @@ public class SQLiteDataHelper extends SQLiteOpenHelper {
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
 
         sqLiteDatabase.
-                execSQL("CREATE TABLE TURMA (id INTEGER PRIMARY KEY, nomeTurma TEXT, anoLetivo INTEGER)");
+                execSQL("CREATE TABLE TURMA (id INTEGER PRIMARY KEY AUTOINCREMENT, nomeTurma TEXT, " +
+                        "anoLetivo INTEGER)");
 
         sqLiteDatabase.
-                execSQL("CREATE TABLE ALUNO (matricula INTEGER PRIMARY KEY, nome TEXT,  cpf TEXT, turmaId INTEGER, " +
+                execSQL("CREATE TABLE ALUNO (matricula INTEGER PRIMARY KEY AUTOINCREMENT, nome TEXT,  cpf TEXT, " +
+                        "turmaId INTEGER, " +
                         "FOREIGN KEY (turmaId) REFERENCES TURMA(id))");
 
         sqLiteDatabase.
-                execSQL("CREATE TABLE PROFESSOR (registro INTEGER PRIMARY KEY, nome TEXT,  cpf TEXT)");
+                execSQL("CREATE TABLE PROFESSOR (registro INTEGER PRIMARY KEY AUTOINCREMENT, nome TEXT,  cpf TEXT)");
 
         sqLiteDatabase.
-                execSQL("CREATE TABLE DISCIPLINA (id INTEGER PRIMARY KEY, nome TEXT, professorRegistro INTEGER, " +
+                execSQL("CREATE TABLE DISCIPLINA (id INTEGER PRIMARY KEY AUTOINCREMENT, nome TEXT, " +
+                        "professorRegistro INTEGER, " +
                         "FOREIGN KEY (professorRegistro) REFERENCES PROFESSOR(registro))");
 
         sqLiteDatabase.
@@ -41,7 +45,8 @@ public class SQLiteDataHelper extends SQLiteOpenHelper {
                         "FOREIGN KEY (disciplinaId) REFERENCES DISCIPLINA(id)," +
                         "FOREIGN KEY (turmaId) REFERENCES TURMA(id))");
 
-        //o invés de BOOLEAN, o SQLite não possui um tipo booleano nativo. Recomenda-se usar INTEGER para valores booleanos (0 para falso e 1 para verdadeiro).
+        //o invés de BOOLEAN, o SQLite não possui um tipo booleano nativo. Recomenda-se usar INTEGER
+        // para valores booleanos (0 para falso e 1 para verdadeiro).
         sqLiteDatabase.
                 execSQL("CREATE TABLE PRESENCA (id INTEGER PRIMARY KEY, data DATE, presente INTEGER, " +
                         "   alunoMatricula INTEGER, disciplinaId INTEGER, " +
@@ -55,7 +60,7 @@ public class SQLiteDataHelper extends SQLiteOpenHelper {
                         "FOREIGN KEY (alunoMatricula) REFERENCES ALUNO(matricula), " +
                         "FOREIGN KEY (disciplinaId) REFERENCES DISCIPLINA(id))");
 
-
+        SeedData.insertInitialData(sqLiteDatabase);
     }
 
     @Override
