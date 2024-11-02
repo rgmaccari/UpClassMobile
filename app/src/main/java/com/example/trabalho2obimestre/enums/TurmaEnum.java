aaebe3d3860f87acf568d3be7710df834bc323ca
Marcelo <guedesmarcello733@outlook.com>
2024-11-02 12:12:28 -0300

    Criado Turma Enum

--- app/src/main/java/com/example/trabalho2obimestre/enums/TurmaEnum.java
+++ app/src/main/java/com/example/trabalho2obimestre/enums/TurmaEnum.java
@@ -1,14 +1,16 @@
 package com.example.trabalho2obimestre.enums;
 
-public enum turmaEnum {
+public enum TurmaEnum {
     PRIMEIRO_ANO_A("1A"),
     PRIMEIRO_ANO_B("1B"),
     SEGUNDO_ANO_A("2A"),
-    SEGUNDO_ANO_B("2B");
+    SEGUNDO_ANO_B("2B"),
+    TERCEIRO_ANO_A("3A"),
+    TERCEIRO_ANO_B("3B");
 
     public final String descricao;
 
-    private turmaEnum(String descricao) {
+    private TurmaEnum(String descricao) {
 
         this.descricao = descricao;
     }
